d9f8379b308f98c24a9ac24de129110fe23a3c0a
Anmol Singh <5581466+anmolsingh95@users.noreply.github.com>
2024-03-24 17:33:13 -0700

    basic protocol mostly working.

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,127 @@
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::str;
 
-fn log_info(str: &str) {
-    #[cfg(debug_assertions)]
-    println!("INFO: {str}");
+// request handlers
+fn handle_set_request(
+    request: SetRequest,
+    cache: &mut HashMap<Vec<u8>, Vec<u8>>,
+) -> std::io::Result<()> {
+    log_info("inside set request handler");
+    cache.insert(request.key, request.data);
+    Ok(())
 }
 
-fn read_until_excluding_delimiter(
-    reader: &mut BufReader<impl std::io::Read>,
-    delimiter: u8,
-    buf: &mut Vec<u8>,
-) -> std::io::Result<usize> {
-    let bytes_read = reader.read_until(delimiter, buf)?;
-    if buf.ends_with(&[delimiter]) {
-        buf.pop();
+fn handle_get_request(
+    request: GetRequest,
+    cache: &mut HashMap<Vec<u8>, Vec<u8>>,
+    mut stream: TcpStream,
+) -> std::io::Result<()> {
+    log_info("inside get request handler");
+    if let Some(value) = cache.get(&request.key) {
+        stream
+            .write_all(
+                format!(
+                    "VALUE {key} 0 {bytes}\r\n",
+                    key = str::from_utf8(&request.key).unwrap(),
+                    bytes = value.len()
+                )
+                .as_bytes(),
+            )
+            .unwrap();
+        stream.write_all(value).unwrap();
+        stream.write_all(b"\r\n").unwrap();
+        send_end(stream);
+    } else {
+        send_end(stream);
     }
-    Ok(bytes_read)
+
+    Ok(())
 }
 
-fn handle_client(stream: TcpStream) -> std::io::Result<()> {
-    let peer_addr = stream.peer_addr().unwrap();
-    log_info(&format!("handling tcpstream from {peer_addr}"));
+fn handle_unknown_request(stream: TcpStream) -> std::io::Result<()> {
+    log_info("inside unknown request handler");
+    send_end(stream);
+    Ok(())
+}
 
-    let mut reader = BufReader::new(&stream);
-    let mut buffer = Vec::new();
-    read_until_excluding_delimiter(&mut reader, b' ', &mut buffer).unwrap();
-    let command = str::from_utf8(&buffer).unwrap();
-    log_info(&format!("received command: '{command}'"));
+fn send_end(mut stream: TcpStream) {
+    stream.write_all("END\r\n".as_bytes()).unwrap();
+}
+
+// Parsing request
+#[derive(Debug)]
+struct GetRequest {
+    key: Vec<u8>,
+}
+
+#[derive(Debug)]
+struct SetRequest {
+    key: Vec<u8>,
+    flags: u32,
+    bytes: Vec<u8>,
+    ttl: u32,
+    noreply: bool,
+    data: Vec<u8>,
+}
+
+#[derive(Debug)]
+enum Request {
+    Get(GetRequest),
+    Set(SetRequest),
+    Unknown,
+}
+
+fn parse_request(stream: &mut TcpStream) -> Request {
+    let mut reader = BufReader::new(stream);
+    let mut command_line = String::new();
+    reader.read_line(&mut command_line).unwrap();
+
+    let mut iter = command_line.split_whitespace();
+    let command = iter.next().unwrap();
     match command {
         "get" => {
-            log_info("get command");
-            handle_get_command(stream)
+            let key = iter.next().unwrap();
+            return Request::Get(GetRequest {
+                key: key.as_bytes().to_vec(),
+            });
         }
         "set" => {
-            log_info("set command");
-            handle_set_command(stream)
+            let key = iter.next().unwrap();
+            let flags = iter.next().unwrap().parse().unwrap();
+            let ttl = iter.next().unwrap().parse().unwrap();
+            let bytes = iter.next().unwrap();
+            let noreply = iter.next().unwrap_or("false") == "noreply";
+            let mut data_block = String::new();
+            reader.read_line(&mut data_block).unwrap();
+            return Request::Set(SetRequest {
+                key: key.as_bytes().to_vec(),
+                flags,
+                bytes: bytes.as_bytes().to_vec(),
+                ttl,
+                noreply,
+                data: data_block.as_bytes().to_vec(),
+            });
         }
-        _ => {
-            log_info("unkonwn command");
-            handle_unknown_command(stream)
-        }
+        _ => Request::Unknown,
+    }
+}
+
+fn handle_connection(
+    mut stream: TcpStream,
+    cache: &mut HashMap<Vec<u8>, Vec<u8>>,
+) -> std::io::Result<()> {
+    let peer_addr = stream.peer_addr().unwrap();
+    log_info(&format!("handling tcpstream from {peer_addr}"));
+    let request = parse_request(&mut stream);
+    log_info(&format!("received request: {request:?}"));
+    match request {
+        Request::Get(get_request) => handle_get_request(get_request, cache, stream),
+        Request::Set(set_request) => handle_set_request(set_request, cache),
+        Request::Unknown => handle_unknown_request(stream),
     }
     .unwrap();
-
-    Ok(())
-}
-
-fn handle_set_command(_stream: TcpStream) -> std::io::Result<()> {
-    log_info("inside set command handler");
-    Ok(())
-}
-
-fn handle_get_command(stream: TcpStream) -> std::io::Result<()> {
-    log_info("inside get command handler");
-    let mut reader = BufReader::new(&stream);
-    let mut buffer = String::new();
-    reader.read_line(&mut buffer)?;
-
-    log_info(&format!("received buffer inside get: {buffer:?}"));
-    let mut split_buffer = buffer.split_whitespace();
-    let _command = split_buffer.next().unwrap();
-    Ok(())
-}
-
-fn handle_unknown_command(mut stream: TcpStream) -> std::io::Result<()> {
-    log_info("inside unknown command handler");
-    stream.write_all("End\r\n".as_bytes()).unwrap();
     Ok(())
 }
 
@@ -75,9 +130,16 @@ fn main() -> std::io::Result<()> {
     let local_addr = listener.local_addr().unwrap();
     log_info(&format!("memcachr listening at {local_addr}"));
 
+    let mut cache: HashMap<Vec<u8>, Vec<u8>> = HashMap::new();
     // accept connections and process them serially
     for stream in listener.incoming() {
-        handle_client(stream?).unwrap();
+        handle_connection(stream.unwrap(), &mut cache).unwrap();
     }
     Ok(())
+}
+
+// Utility functions
+fn log_info(str: &str) {
+    #[cfg(debug_assertions)]
+    println!("INFO: {str}");
 }
